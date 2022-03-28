f72ea48b6ba69387a340988f44f2fa53dec533d1
Greysonnn <25331809+Greysonnn@users.noreply.github.com>
2022-03-28 11:01:59 -0600

    Update create-detailed-table.sql

--- create-detailed-table.sql
+++ create-detailed-table.sql
@@ -1,8 +1,20 @@
-CREATE TABLE customer_rentals (
-  customer_id INT,
-  active INT,
-  rental_id INT,
-  staff_id INT
+CREATE TABLE detailed_table  AS (
+SELECT 
+  c.customer_id,
+  c.first_name,
+  c.last_name,
+  c.email,
+  rental.return_date,
+  CASE
+    WHEN rental.return_date >= ‘2005-08-07’ AND rental.return_date <= ‘2005-08-14’ THEN ‘Past-due’
+  END late_rental
+FROM rental
+JOIN customer c ON c.customer_id = rental.customer_id
+GROUP BY 
+  c.customer_id,
+  return_date,
+  first_name 
 );
+
 
 SELECT * FROM customer_rentals
