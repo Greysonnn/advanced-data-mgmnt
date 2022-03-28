e4d0e2e1f8838924f66ce6ecb941ce983e0bb1f8
Greysonnn <25331809+Greysonnn@users.noreply.github.com>
2022-03-28 11:02:35 -0600

    Update create-summary-table.sql

--- create-summary-table.sql
+++ create-summary-table.sql
@@ -1,8 +1,8 @@
-CREATE TABLE active_rentals (
-  customer_id INT,
-  active VARCHAR(25),
-  staff_id INT,
-  total_rentals INT
+CREATE TABLE summary_table (
+  Customer_id INT,
+  Full_name VARCHAR(50),	
+  Email VARCHAR(50),
+  Late_rental TEXT 
 );
 
 SELECT * FROM active_rentals 
